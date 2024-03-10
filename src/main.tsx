c216b073e189067cdc4433e76d4a56c0fe451264
nanpangyou <yigehd@foxmail.com>
2024-03-10 14:04:50 +0800

    remove useless template

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const div = document.getElementById("root");
+
+const root = ReactDOM.createRoot(div!);
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
