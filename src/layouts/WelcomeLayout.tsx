c291609955fcefd6f6bd7c73a6eb37869c6c15e8
nanpangyou <yigehd@foxmail.com>
2024-03-18 23:27:13 +0800

    debuging

--- src/layouts/WelcomeLayout.tsx
+++ src/layouts/WelcomeLayout.tsx
@@ -1,5 +1,22 @@
-import { Outlet } from "react-router-dom"
+import { animated, useTransition } from "@react-spring/web"
+import { type ReactNode, useRef } from "react"
+import { Outlet, useLocation } from "react-router-dom"
 
 export const WelcomeLayout: React.FC = () => {
-  return <div><Outlet /></div>
+  const pathNames = useLocation()
+  const map = useRef<Record<string, ReactNode>>({})
+  const transitions = useTransition(pathNames, {
+    from: { transform: "translateX(100%)" },
+    enter: { transform: "translateX(0%)" },
+    leave: { transform: "translateX(-100%)" },
+    config: { duration: 3000 },
+  })
+  return transitions((style, pathname) => (
+    <animated.div style={{ textAlign: "center", ...style }}>
+      {/* {{ pathname }} */}
+      <Outlet />
+    </animated.div>
+  ))
+  // console.log(pathNames)
+  // return <Outlet />
 }
