ea0feaaec9826fe1b5734bb7852ddbbf28c7e346
nanpangyou <yigehd@foxmail.com>
2024-03-19 15:37:39 +0800

    添加路由切换动画

--- src/layouts/WelcomeLayout.tsx
+++ src/layouts/WelcomeLayout.tsx
@@ -1,22 +1,21 @@
 import { animated, useTransition } from "@react-spring/web"
 import { type ReactNode, useRef } from "react"
-import { Outlet, useLocation } from "react-router-dom"
+import { useLocation, useOutlet } from "react-router-dom"
 
 export const WelcomeLayout: React.FC = () => {
-  const pathNames = useLocation()
+  const location = useLocation()
+  const outlet = useOutlet()
   const map = useRef<Record<string, ReactNode>>({})
-  const transitions = useTransition(pathNames, {
-    from: { transform: "translateX(100%)" },
+  map.current[location.pathname] = outlet
+  const transitions = useTransition(location.pathname, {
+    from: { transform: location.pathname === "/welcome/1" ? "translateX(0%)" : "translateX(100%)" },
     enter: { transform: "translateX(0%)" },
     leave: { transform: "translateX(-100%)" },
-    config: { duration: 3000 },
+    config: { duration: 300 },
   })
   return transitions((style, pathname) => (
     <animated.div style={{ textAlign: "center", ...style }}>
-      {/* {{ pathname }} */}
-      <Outlet />
+      {map.current[pathname]}
     </animated.div>
   ))
-  // console.log(pathNames)
-  // return <Outlet />
 }
--- src/pages/Welcome1.tsx
+++ src/pages/Welcome1.tsx
@@ -1,11 +1,9 @@
 import React from "react"
 import { NavLink } from "react-router-dom"
-import { Demo } from "../demo"
 
 export const Welcome1: React.FC = () => {
   return (
     <div style={{ border: "1px solid red" }}>
-      <Demo></Demo>
       1
       <NavLink to="/welcome/2">下一页</NavLink>
     </div>
